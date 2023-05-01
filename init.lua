4939bbc341f8acad64a9807b00a3975e93d32587
Luke Wiebe <mail@lukewiebe.com>
2023-05-01 11:05:22 -0500

    remove packer.nvim bootstrap code

--- init.lua
+++ init.lua
@@ -35,27 +35,10 @@ end
 
 -- [[ PLUGINS ]]
 -- using packer.nvim
--- PROBLEMS: lua code doesn't work after this section.
-
--- bootstrap copypasta from https://github.com/wbthomason/packer.nvim
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   -- let packer manage itself
   use 'wbthomason/packer.nvim'
-
-  -- [[ Pause CopyPasta ]]
 
   -- required for me to feel sane
   use 'cohama/lexima.vim'
@@ -66,13 +49,4 @@ return require('packer').startup(function(use)
 
   use 'neovim/nvim-lspconfig'
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-
-  -- [[ Resume CopyPasta ]]
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
 end)
-
--- /copypasta
