e309a5074d51f2cdbf339c4de0da0c054cc7dafb
Luke Wiebe <mail@lukewiebe.com>
2023-05-01 13:44:09 -0500

    add LSP keymaps in an LSPAttach augroup

--- init.lua
+++ init.lua
@@ -52,3 +52,29 @@ require('packer').startup(function(use)
   use 'https://github.com/neovim/nvim-lspconfig'
   use 'https://github.com/mfussenegger/nvim-jdtls'
 end)
+
+-- Global LSP configuration
+vim.diagnostic.disable() -- turn off all diagnostics.
+-- use vim.diagnostic.enable() to restart them.
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<Leader>s', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<Leader>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
