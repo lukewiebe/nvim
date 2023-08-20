9ee3d0ae6e7793c30ee36576a82b064dbff00c27
lukewiebe <mail@lukewiebe.com>
2023-08-20 14:04:25 -0500

    update

--- plugin/packer.lua
+++ plugin/packer.lua
@@ -34,6 +34,5 @@ require('packer').startup(function(use)
   use 'https://github.com/neovim/nvim-lspconfig' -- sensible default LSP configs
   use 'https://github.com/mfussenegger/nvim-jdtls' -- really good Java LSP integration
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use 'https://github.com/sbdchd/neoformat'
 
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -94,11 +94,6 @@ _G.packer_plugins = {
     path = "/Users/luke/.local/share/nvim/site/pack/packer/start/lexima.vim",
     url = "https://github.com/cohama/lexima.vim"
   },
-  neoformat = {
-    loaded = true,
-    path = "/Users/luke/.local/share/nvim/site/pack/packer/start/neoformat",
-    url = "https://github.com/sbdchd/neoformat"
-  },
   ["nvim-jdtls"] = {
     loaded = true,
     path = "/Users/luke/.local/share/nvim/site/pack/packer/start/nvim-jdtls",
