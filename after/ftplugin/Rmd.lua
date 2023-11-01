cad37a96ad425f8eab4d2f01f3866b1f0ba5e73e
lukewiebe <mail@lukewiebe.com>
2023-11-01 12:28:48 -0500

    Update run keybindings for .Rmd

--- after/ftplugin/Rmd.lua
+++ after/ftplugin/Rmd.lua
@@ -1,2 +1,3 @@
 require("prose")
-vim.keymap.set("n", "<leader>r", "<cmd>w<CR>:!~/sd/r/render %<CR>", { buffer = true })
+vim.keymap.set("n", "<leader>r", "<cmd>w<CR>:!~/sd/r/render2html %<CR>", { buffer = true })
+vim.keymap.set("n", "<leader>t", "<cmd>w<CR>:!~/sd/r/render2pdf %<CR>", { buffer = true })
