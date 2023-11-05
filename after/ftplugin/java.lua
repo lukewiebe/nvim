8ca51ca34356da820d26ee15ad1ee59394cb5503
lukewiebe <mail@lukewiebe.com>
2023-11-05 13:17:07 -0600

    Update

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -3,10 +3,9 @@
 -- Commands to run code
 vim.keymap.set('n', '<Leader>r', "<cmd>w<CR>:!java %<CR>", { buffer = true })
 vim.keymap.set('n', '<Leader>t', "<cmd>w<CR>:terminal java %<CR>i", { buffer = true })
-vim.bo.tabstop = 4
 
 -- configure nvim-jdtls
 require('jdtls').start_or_attach({
     cmd = {'/opt/homebrew/bin/jdtls'},
-    root_dir = vim.fs.dirname(vim.fn.getcwd()),
+    root_dir = vim.fn.getcwd(),
 })
