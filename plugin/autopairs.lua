15ff8b682354ce54e08c27ac7afd3f92a1e542bf
lukewiebe <mail@lukewiebe.com>
2023-11-15 12:57:49 -0600

    Add spaces between brackets functionality to autopairs

--- plugin/autopairs.lua
+++ plugin/autopairs.lua
@@ -9,3 +9,50 @@ local cond = require('nvim-autopairs.conds')
 -- the :with_move() addition allows 'type over' behaviour
 -- cond.done() always returns true, but we can specify conditions
 npairs.add_rule(Rule("<", ">", "html"):with_move(cond.done()))
+-- this rule is obviated by nvim-ts-autotag, but kept for reference
+
+-- the following replicates the spaces-between-brackets behaviour I like so much.
+-- copied from https://github.com/windwp/nvim-autopairs/wiki/Custom-rules
+
+local brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
+npairs.add_rules {
+  -- Rule for a pair with left-side ' ' and right side ' '
+  Rule(' ', ' ')
+    -- Pair will only occur if the conditional function returns true
+    :with_pair(function(opts)
+      -- We are checking if we are inserting a space in (), [], or {}
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({
+        brackets[1][1] .. brackets[1][2],
+        brackets[2][1] .. brackets[2][2],
+        brackets[3][1] .. brackets[3][2]
+      }, pair)
+    end)
+    :with_move(cond.none())
+    :with_cr(cond.none())
+    -- We only want to delete the pair of spaces when the cursor is as such: ( | )
+    :with_del(function(opts)
+      local col = vim.api.nvim_win_get_cursor(0)[2]
+      local context = opts.line:sub(col - 1, col + 2)
+      return vim.tbl_contains({
+        brackets[1][1] .. '  ' .. brackets[1][2],
+        brackets[2][1] .. '  ' .. brackets[2][2],
+        brackets[3][1] .. '  ' .. brackets[3][2]
+      }, context)
+    end)
+}
+-- For each pair of brackets we will add another rule
+for _, bracket in pairs(brackets) do
+  npairs.add_rules {
+    -- Each of these rules is for a pair with left-side '( ' and right-side ' )' for each bracket type
+    Rule(bracket[1] .. ' ', ' ' .. bracket[2])
+      :with_pair(cond.none())
+      :with_move(function(opts) return opts.char == bracket[2] end)
+      :with_del(cond.none())
+      :use_key(bracket[2])
+      -- Removes the trailing whitespace that can occur without this
+      :replace_map_cr(function(_) return '<C-c>2xi<CR><C-c>O' end)
+  }
+end
+
+-- end copy
