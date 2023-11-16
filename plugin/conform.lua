06925b62b3871fd803dd0b7f31b42d6a1b662518
lukewiebe <mail@lukewiebe.com>
2023-11-16 13:09:18 -0600

    update

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -15,3 +15,6 @@ require("conform").setup({
 
 -- this is a way to use conform's formatter
 -- vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+-- to ignore parts of a file using stylua:
+-- https://github.com/JohnnyMorganz/StyLua#ignoring-parts-of-a-file
