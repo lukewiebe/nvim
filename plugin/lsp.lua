f6319f91008e8c37f6b3c186f78a60c5923a09b7
lukewiebe <mail@lukewiebe.com>
2023-11-16 13:08:36 -0600

    Tidy LSP config

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -11,16 +11,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		-- Buffer local mappings
 
-		local opts = { buffer = args.buf }
 		if client.server_capabilities.hoverProvider then
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
 		end
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		-- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = args.buf })
 		-- gd provided by treesitter with LSP fallback
-		-- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "<space>gi", vim.lsp.buf.implementation, opts)
-		-- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-		vim.keymap.set("n", "<space>r", vim.rsp.buf.rename, opts)
-		vim.keymap.set("n", "<space>gr", vim.lsp.buf.references, opts)
+		-- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf })
+		vim.keymap.set("n", "<space>gi", vim.lsp.buf.implementation, { buffer = args.buf })
+		-- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { buffer = args.buf })
+		vim.keymap.set("n", "<space>r", vim.rsp.buf.rename, { buffer = args.buf })
+		vim.keymap.set("n", "<space>gr", vim.lsp.buf.references, { buffer = args.buf })
 	end,
 })
