97a67accf7dbdd0e53f4c465eb9985ec0cb5f89f
lukewiebe <mail@lukewiebe.com>
2023-12-02 15:50:10 -0600

    Colorscheme: add highlight modifier function and italicize comments
    Now I have a nice way to tweak colorschemes in Lua, without falling
    back to VimScript.

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -2,10 +2,19 @@
 vim.o.termguicolors = true
 
 vim.cmd.colorscheme("spaceduck")
--- tweak some color choices
--- nvim_set_hl() wipes out the color group, so I need to rebuild the parts I want to keep manually
--- TODO: redo this with vimscript commands, they're much better behaved
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none", fg = "#30365F" })
-vim.api.nvim_set_hl(0, "LineNr", { bg = "none", fg = "#30365F" })
+-- borrowed from https://stackoverflow.com/questions/74677587/how-to-set-comments-to-italic-fonts-in-neovim
+local update_highlight = function(group_name, opts)
+	local hl_group = vim.api.nvim_get_hl(0, { name = group_name })
+	hl_group = vim.tbl_extend("force", hl_group, opts)
+	vim.api.nvim_set_hl(0, group_name, hl_group)
+end
+
+update_highlight("Comment", { italic = true })
+update_highlight("Normal", { bg = "none" })
+update_highlight("EndOfBuffer", { bg = "none" })
+update_highlight("LineNr", { bg = "none" })
+
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none", fg = "#30365F" })
+-- vim.api.nvim_set_hl(0, "LineNr", { bg = "none", fg = "#30365F" })
