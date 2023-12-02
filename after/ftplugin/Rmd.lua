52cfb244c302e83d2ec6fd255f08080805a6dbf2
lukewiebe <mail@lukewiebe.com>
2023-12-02 13:28:30 -0600

    Add autopair for $ in .Rmd files

--- after/ftplugin/Rmd.lua
+++ after/ftplugin/Rmd.lua
@@ -1,3 +1,9 @@
 require("prose")
 vim.keymap.set("n", "<leader>r", "<cmd>w<CR>:!~/sd/r/render2html %<CR>", { buffer = true })
 vim.keymap.set("n", "<leader>t", "<cmd>w<CR>:!~/sd/r/render2pdf %<CR>", { buffer = true })
+
+-- from https://github.com/windwp/nvim-autopairs
+local Rule = require("nvim-autopairs.rule")
+local npairs = require("nvim-autopairs")
+local cond = require("nvim-autopairs.conds")
+npairs.add_rule(Rule("$", "$", "rmd"):with_move(cond.done()))
