392b3dd5b001427d39087fbd7e202f2de0f4ecb1
lukewiebe <mail@lukewiebe.com>
2024-01-23 10:12:19 -0600

    Change comment-at-end-of-line binding from gcA to gca

--- plugin/comment-nvim.lua
+++ plugin/comment-nvim.lua
@@ -1,6 +1,7 @@
 -- https://github.com/numToStr/Comment.nvim
 
 require("Comment").setup({
+	extra = { eol = "gca" },
 	-- integrate with ts_context_commentstring as per
 	-- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#commentnvim
 	pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
