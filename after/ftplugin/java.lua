585095a94a5386ff4eb092f0f02dd143f80ff758
lukewiebe <mail@lukewiebe.com>
2024-01-26 14:09:38 -0600

    undo java % makeprg for java files
    If I need to run `java %` through :make, it's easy enough to set
    makeprg for a one-off run. It's better to keep :make pointed at real
    make, so I can use it for more demanding projects.

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -4,8 +4,6 @@
 vim.keymap.set("n", "<Leader>r", "<cmd>w<CR>:!java %<CR>", { buffer = true })
 vim.keymap.set("n", "<Leader>t", "<cmd>w<CR>:terminal java %<CR>i", { buffer = true })
 
-vim.bo.makeprg = "java %"
-
 -- configure nvim-jdtls
 require("jdtls").start_or_attach({
 	cmd = { "/opt/homebrew/bin/jdtls" },
