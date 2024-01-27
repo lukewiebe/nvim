91b2c2bbda0ddb598ec553379db39cb4ab317e50
lukewiebe <mail@lukewiebe.com>
2024-01-27 11:54:26 -0600

    Fix statusline and window border colors

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -1,4 +1,4 @@
---  COLORS --
+--  COLORS
 vim.o.termguicolors = true
 
 vim.cmd.colorscheme("spaceduck")
@@ -6,10 +6,13 @@ vim.cmd.colorscheme("spaceduck")
 local update_highlight = require("update_highlight")
 
 update_highlight("Comment", { italic = true })
+
+-- remove backgrounds from most things
 update_highlight("Normal", { bg = "none" })
 update_highlight("EndOfBuffer", { bg = "none" })
 update_highlight("LineNr", { bg = "none" })
 
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none", fg = "#30365F" })
--- vim.api.nvim_set_hl(0, "LineNr", { bg = "none", fg = "#30365F" })
+-- status bar
+vim.api.nvim_set_hl(0, "StatusLine", { fg = "#b3a1e6", bg = "#0f111b" })
+vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "#686f9a", bg = "#0f111b" })
+vim.api.nvim_set_hl(0, "VertSplit", { fg = "#0f111b", bg = "#0f111b" })
