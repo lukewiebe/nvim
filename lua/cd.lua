905c81b8f0b43606f45fd791dd4bea17ee72afbc
lukewiebe <mail@lukewiebe.com>
2024-02-16 13:44:48 -0600

    Add oil support for :Cd
    Use the same logic as :Finder to account for Oil buffer paths.

--- lua/cd.lua
+++ lua/cd.lua
@@ -1,3 +1,7 @@
 -- :Cd to change working dir to dir of current buffer
 
-vim.api.nvim_create_user_command("Cd", "cd %:h", {})
+vim.api.nvim_create_user_command("Cd", function(args)
+	local dir = vim.fn.expand("%:h")
+	dir = string.gsub(dir, "^oil://(.*)", "%1")
+	vim.cmd.cd(dir)
+end, {})
