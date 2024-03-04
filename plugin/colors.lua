abf69480b1a8170e762da617ac2357c7b0bdba51
lukewiebe <mail@lukewiebe.com>
2024-03-04 09:39:18 -0600

    Modify highlight tools
    update_highlight is now update_hl and is returned as a method instead of
    directly. This will allow me to add more utilities if I need to.

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -3,16 +3,16 @@ vim.o.termguicolors = true
 
 vim.cmd.colorscheme("spaceduck")
 
-local update_highlight = require("update_highlight")
+local update_hl = require("highlights").update_hl
 
-update_highlight("Comment", { italic = true })
+update_hl("Comment", { italic = true })
 
 -- remove backgrounds from most things
-update_highlight("Normal", { bg = "none" })
-update_highlight("EndOfBuffer", { bg = "none" })
-update_highlight("LineNr", { bg = "none" })
-update_highlight("CursorLineNr", { bg = "none" })
-update_highlight("CursorLine", { bg = "none" })
+update_hl("Normal", { bg = "none" })
+update_hl("EndOfBuffer", { bg = "none" })
+update_hl("LineNr", { bg = "none" })
+update_hl("CursorLine", { bg = "none" })
+update_hl("CursorLineNr", { bg = "none" })
 
 -- status bar and window trim
 vim.api.nvim_set_hl(0, "StatusLine", { fg = "#b3a1e6", bg = "#0f111b" })
