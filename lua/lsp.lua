dac60065c4adaec0adb9b3e4d85a364bfa9a8b93
lukewiebe <mail@lukewiebe.com>
2024-03-04 09:46:36 -0600

    Update lsp keymaps
    Remove some unused ones and change <space>gr to just gr for references

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,7 +1,7 @@
 -- Global LSP Configuration --
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+	group = vim.api.nvim_create_augroup("lsp-keymaps", { clear = true }),
 	callback = function(args)
 		-- enable LSP omnifunc
 		vim.bo[args.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
@@ -11,14 +11,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		if client.server_capabilities.hoverProvider then
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
 		end
-		-- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = args.buf })
-		-- gd provided by treesitter with LSP fallback
-		-- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf })
-		vim.keymap.set("n", "<space>gi", vim.lsp.buf.implementation, { buffer = args.buf })
-		-- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { buffer = args.buf })
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = args.buf })
 		vim.keymap.set("n", "<leader>s", vim.lsp.buf.rename, { buffer = args.buf })
-		vim.keymap.set("n", "<space>gr", vim.lsp.buf.references, { buffer = args.buf })
-
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = args.buf })
+		--
 		-- disable syntax highlight, just use treesitter for that
 		client.server_capabilities.semanticTokensProvider = nil
 	end,
