5567b11547e5d1a683e55be1e347afe67e3624e6
lukewiebe <mail@lukewiebe.com>
2024-03-04 09:40:38 -0600

    New setup for gx.nvim
    Also removed search fallback because I never want that

--- plugin/gx.lua
+++ plugin/gx.lua
@@ -1,3 +1,9 @@
 -- https://github.com/chrishrb/gx.nvim
 
-require("gx").setup({})
+require("gx").setup({
+	handlers = {
+		search = false,
+	},
+})
+
+vim.keymap.set({ "n", "v" }, "gx", "<cmd>Browse<cr>", { silent = true })
