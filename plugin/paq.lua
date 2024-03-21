d82175ae172c45ea73634b49f58cbf27167bf809
lukewiebe <mail@lukewiebe.com>
2024-03-21 13:12:48 -0500

    Add todo-comments plugin

--- plugin/paq.lua
+++ plugin/paq.lua
@@ -44,4 +44,5 @@ require("paq")({
 	"https://github.com/tpope/vim-rhubarb", -- github addition to fugitive
 	"https://github.com/dcampos/nvim-snippy", -- minimal snippet plugin
 	"https://github.com/NvChad/nvim-colorizer.lua", -- show tailwind colors inline
+	"https://github.com/folke/todo-comments.nvim",
 })
---
+++ plugin/todo-comments.lua
@@ -0,0 +1,6 @@
+-- https://github.com/folke/todo-comments.nvim
+
+require("todo-comments").setup({
+	signs = false,
+})
+-- TODO:
