90e230339537bc9b4fbb28fcb8b60f1e71baa08f
lukewiebe <mail@lukewiebe.com>
2024-03-25 10:55:45 -0500

    Rmd config changes
    Swap keybindings for rendering to pdf and html, and remove highlight
    changes since they don't seem to be a problem anymore.

--- after/ftplugin/Rmd.lua
+++ after/ftplugin/Rmd.lua
@@ -1,6 +1,3 @@
 require("prose")
-vim.keymap.set("n", "<leader>r", "<cmd>w<CR>:!~/sd/r/render2html %<CR>", { buffer = true })
-vim.keymap.set("n", "<leader>t", "<cmd>w<CR>:!~/sd/r/render2pdf %<CR>", { buffer = true })
-
--- remove italics on comments, it also hits code blocks apparently
-require("update_highlight")("Comment", { italic = false })
+vim.keymap.set("n", "<leader>r", "<cmd>w<CR>:!~/sd/r/render2pdf %<CR>", { buffer = true })
+vim.keymap.set("n", "<leader>t", "<cmd>w<CR>:!~/sd/r/render2html %<CR>", { buffer = true })
