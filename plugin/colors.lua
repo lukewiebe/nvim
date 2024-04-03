0c1c64b220ba4c9266a46458c5787e3d3f44721e
lukewiebe <mail@lukewiebe.com>
2024-04-03 12:34:55 -0500

    Wrap colorscheme modifications in an autocmd

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -1,16 +1,27 @@
 --  COLORS
 vim.o.termguicolors = true
-vim.cmd.colorscheme("spaceduck")
 
-local update_hl = require("highlights").update_hl
-update_hl("Comment", { italic = true })
--- remove backgrounds from most things
-update_hl("Normal", { bg = "none" })
-update_hl("EndOfBuffer", { bg = "none" })
-update_hl("LineNr", { bg = "none" })
-update_hl("CursorLine", { bg = "none" })
-update_hl("CursorLineNr", { bg = "none" })
--- status bar and window trim
-vim.api.nvim_set_hl(0, "StatusLine", { fg = "#b3a1e6", bg = "#0f111b" })
-vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "#686f9a", bg = "#0f111b" })
-vim.api.nvim_set_hl(0, "VertSplit", { fg = "#0f111b", bg = "#0f111b" })
+local edit_colorscheme = function()
+	local update_hl = require("highlights").update_hl
+	update_hl("Comment", { italic = true })
+	-- remove backgrounds from most things
+	update_hl("Normal", { bg = "none" })
+	update_hl("EndOfBuffer", { bg = "none" })
+	update_hl("LineNr", { bg = "none" })
+	update_hl("CursorLine", { bg = "none" })
+	update_hl("CursorLineNr", { bg = "none" })
+	-- status bar and window trim
+	vim.api.nvim_set_hl(0, "StatusLine", { fg = "#b3a1e6", bg = "#0f111b" })
+	vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "#686f9a", bg = "#0f111b" })
+	vim.api.nvim_set_hl(0, "VertSplit", { fg = "#0f111b", bg = "#0f111b" })
+end
+
+-- autocmd fires when spaceduck is activated
+vim.api.nvim_create_autocmd("ColorScheme", {
+	group = vim.api.nvim_create_augroup("EditColorScheme", { clear = true }),
+	pattern = "spaceduck",
+	callback = edit_colorscheme,
+})
+
+-- now activate the colorscheme
+vim.cmd.colorscheme("spaceduck")
