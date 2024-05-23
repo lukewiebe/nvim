feb96e3d5581578a98bc82547e079a45e1ea4a29
lukewiebe <mail@lukewiebe.com>
2024-05-23 14:31:29 -0500

    Update treesitter config
    Remove unused [u and ]u bindings
    stop ensuring anything is installed, auto install works
    add gO map for quickfix list of definitions

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -8,39 +8,11 @@ vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 -- https://github.com/nvim-treesitter/nvim-treesitter
 
 require("nvim-treesitter.configs").setup({
-	ensure_installed = {
-		-- Add new languages here
-		-- Source of language names: https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
-		"markdown",
-		"comment",
-		"java",
-		"python",
-		"javascript",
-		"css",
-		"html",
-		"lua",
-		"dockerfile",
-		"json",
-		"nix",
-		"bash",
-		"git_rebase",
-		"gitcommit",
-		"gitignore",
-		"git_config",
-		"jq",
-		"regex",
-		"sql",
-		"vim",
-		"r",
-	},
-
 	auto_install = true,
-
 	highlight = {
 		enable = true,
 		-- optimizations can be applied here if needed
 	},
-
 	indent = {
 		enable = true,
 	},
@@ -69,6 +41,7 @@ require("nvim-treesitter.configs").setup({
 			selection_modes = {
 				-- default is 'v'
 				["@function.outer"] = "V",
+				["@class.outer"] = "V",
 			},
 		},
 
@@ -93,26 +66,17 @@ require("nvim-treesitter.configs").setup({
 
 	-- from https://github.com/nvim-treesitter/nvim-treesitter-refactor
 	refactor = {
-		highlight_current_scope = { enable = false }, -- not visually nice
-		highlight_definitions = {
-			enable = false,
-			clear_on_cursor_move = true,
-		},
 		smart_rename = {
 			enable = true,
 			keymaps = {
-				smart_rename = "<leader>s",
+				smart_rename = "<leader>s", -- overridden by autocmd on LSP attach
 			},
 		},
 		navigation = {
 			enable = true,
 			keymaps = {
 				goto_definition_lsp_fallback = "gd",
-				goto_definition = false,
-				list_definitions = false,
-				list_definitions_toc = false,
-				goto_next_usage = "]u",
-				goto_previous_usage = "[u",
+				list_definitions_toc = "gO",
 			},
 		},
 	},
