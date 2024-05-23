60f8ff75952aafd90f893808fb35dfba0f856c96
lukewiebe <mail@lukewiebe.com>
2024-05-23 13:32:40 -0500

    Cleanup
    Inline update_hl because it's only being used in one place
    Add a nil check to lsp keymaps because diagnostics told me to
    Remove make.lua, it was a bad idea

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -8,6 +8,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 		-- Buffer local mappings
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client == nil then return end
 		if client.server_capabilities.hoverProvider then
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
 		end
--- plugin/colors.lua
+++ plugin/colors.lua
@@ -1,6 +1,11 @@
 vim.o.termguicolors = true
 
-local update_hl = require("highlights").update_hl
+-- inspired by https://stackoverflow.com/questions/74677587/how-to-set-comments-to-italic-fonts-in-neovim
+local update_hl = function(group_name, opts)
+	local hl_group = vim.api.nvim_get_hl(0, { name = group_name })
+	hl_group = vim.tbl_extend("force", hl_group, opts)
+	vim.api.nvim_set_hl(0, group_name, hl_group)
+end
 
 -- remove background
 update_hl("Normal", { bg = "none" })
