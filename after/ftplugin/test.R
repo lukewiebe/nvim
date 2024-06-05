1176d3847ca4ad15698d734a8e00435a081b2d73
lukewiebe <mail@lukewiebe.com>
2024-06-04 19:10:27 -0500

    Arty: text-based R console
    The goal of this project is to use httpgd with a long-running R console.
    That's not working yet, but the basics are here!
    
    - :ArtyStart spins up an R interpreter using the new vim.system API
    - :ArtyStop closes the interpreter by closing its stdin
    - :ArtyRun sends one-off lines of R, sort of like a console
    - :ArtyWrite sends the whole current buffer to the interpreter
    
    Still left to do:
    - Fix the bug with httpgd that prevents it from displaying images when
      run from Arty

---
+++ after/ftplugin/test.R
@@ -0,0 +1,10 @@
+print("hello world")
+
+x = 1
+y = 2
+x + y
+
+x = 1:1000
+y = sort(rnorm(1000))
+plot(x, y)
+hgd_details()
---
+++ lua/arty.lua
@@ -0,0 +1,61 @@
+-- Arty: a background-process R console for persisting state between script runs
+
+-- working: write a buffer to Arty
+-- not working: get Arty to generate a chart
+
+local cmd = { "R", "--slave", "--no-save" }
+-- local cmd = { "cat" }
+
+-- used by vim.system to print process info on exit
+local on_exit = function(obj)
+	print("### Process exited ###")
+	print("code: " .. obj.code)
+	print("signal: " .. obj.signal)
+	print("stdout: " .. obj.stdout)
+	print("stderr: " .. obj.stderr)
+end
+
+-- prints process stdout and stderr to vim's console
+local print_output = function(err, data)
+	if err then print(err) end
+	if data then print(data) end
+end
+
+-- vim.system options
+local options = {
+	text = true,
+	stdin = true,
+	stdout = print_output,
+	stderr = print_output,
+}
+
+-- starts R console process with httpgd graphics server
+vim.api.nvim_create_user_command("ArtyStart", function()
+	ARTY_SYSTEM_OBJ = vim.system(cmd, options, on_exit)
+
+	ARTY_SYSTEM_OBJ:write("print('foo bar')\n") -- DEBUG should print to messages
+	-- ARTY_SYSTEM_OBJ:write("flush.console()\n")
+	ARTY_SYSTEM_OBJ:write("library('httpgd')\n")
+	ARTY_SYSTEM_OBJ:write("hgd()\n")
+	ARTY_SYSTEM_OBJ:write("hgd_browse()\n")
+end, {})
+
+vim.api.nvim_create_user_command("ArtyStop", function()
+	-- kill Arty's process
+	ARTY_SYSTEM_OBJ:write(nil)
+end, {})
+
+vim.api.nvim_create_user_command("ArtyRun", function(opts)
+	-- write R code to Arty
+	ARTY_SYSTEM_OBJ:write(opts.args .. "\n")
+end, { nargs = "*" })
+
+vim.api.nvim_create_user_command("ArtyWrite", function(opts)
+	-- write R code to Arty from the current buffer
+	local buf = vim.api.nvim_get_current_buf()
+	local buffer_contents = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+	for _, line in ipairs(buffer_contents) do
+		ARTY_SYSTEM_OBJ:write(line .. "\n")
+		-- print(line)
+	end
+end, {})
