add869ccdead0e844915c2f2fc7ad785ec2e8c73
lukewiebe <mail@lukewiebe.com>
2024-10-14 16:07:19 -0500

    Remove autoformat, add <space>f format shortcut
    This is the follow-up to an incredibly annoying incident involving
    autoformatting and the Zig default filetype plugin. I've decided I want
    fewer things happening automatically on save.

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -1,10 +1,5 @@
 -- https://github.com/stevearc/conform.nvim
 
--- copied configuration from
--- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#automatically-run-slow-formatters-async
--- for running slow formatters async (i.e. Java)
-local slow_filetypes = {}
-
 require("conform").setup({
 	formatters_by_ft = {
 		javascript = { "prettier" },
@@ -17,39 +12,10 @@ require("conform").setup({
 		lua = { "stylua" },
 		c = { "clang-format" },
 		fish = { "fish_indent" },
+		zig = { "zigfmt" },
 	},
-
-	format_on_save = function(buf)
-		if slow_filetypes[vim.bo[buf].filetype] then return end
-		local on_format = function(err)
-			-- stylua: ignore
-			if err and err:match("timeout$") then
-				slow_filetypes[vim.bo[buf].filetype] = true
-			end
-		end
-		return { timeout_ms = 200, lsp_fallback = true }, on_format
-	end,
-	format_after_save = function(buf)
-		if not slow_filetypes[vim.bo[buf].filetype] then return end
-		return { lsp_fallback = true }
-	end,
 })
 
--- source: https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
-vim.api.nvim_create_user_command("ConformDisable", function(args)
-	if args.bang then
-		vim.b.disable_autoformat = true
-	else
-		vim.g.disable_autoformat = true
-	end
-end, {
-	desc = "Toggle autoformat on save",
-	bang = true,
-})
+vim.keymap.set("n", "<space>f", require("conform").format, {})
 
-vim.api.nvim_create_user_command("ConformEnable", function(args)
-	vim.b.disable_autoformat = false
-	vim.g.disable_autoformat = false
-end, {
-	desc = "Re-enable autoformat on save",
-})
+vim.g.zig_fmt_autosave = 0 -- incredibly annoying default, can be removed in nvim >0.11
