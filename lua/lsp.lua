64d61971af68f4236716efee57619dd270d9854c
lukewiebe <mail@lukewiebe.com>
2025-03-20 10:13:42 -0500

    Update LSP keymaps to match future nvim 0.11 defaults

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,4 +1,4 @@
--- Global LSP Configuration --
+-- global LSP config
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("LspKeymaps", { clear = true }),
@@ -6,15 +6,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		-- Buffer local mappings
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if client == nil then return end
+
 		if client.server_capabilities.hoverProvider then
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
 		end
-		if client.server_capabilities.codeActionsProvider then
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action(), { buffer = args.buf })
+		if client.server_capabilities.codeActionProvider then
+			vim.keymap.set({"n", "v"}, "gra", vim.lsp.buf.code_action, { buffer = args.buf })
 		end
-		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = args.buf })
-		vim.keymap.set("n", "<leader>s", vim.lsp.buf.rename, { buffer = args.buf })
+		vim.keymap.set({"i", "s"}, "<C-S>", vim.lsp.buf.signature_help, { buffer = args.buf })
+		vim.keymap.set("n", "grn", vim.lsp.buf.rename, { buffer = args.buf })
 		vim.keymap.set("n", "grr", vim.lsp.buf.references, { buffer = args.buf })
+		vim.keymap.set("n", "gri", vim.lsp.buf.implementation, { buffer = args.buf })
 
 		-- disable syntax highlight, just use treesitter for that
 		client.server_capabilities.semanticTokensProvider = nil
