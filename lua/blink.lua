d5556b9c8b3251af6af86fb75fe858ea8ae2cd6c
lukewiebe <mail@lukewiebe.com>
2025-03-20 10:54:07 -0500

    Remove not_in_comment function from blink.lua
    I'll want that function at some point, but it's not consistently
    working, and it belongs in a resuable module.

--- lua/blink.lua
+++ lua/blink.lua
@@ -1,16 +1,5 @@
-local not_in_comment = function()
-	local parser = vim.treesitter.get_parser()
-	local trees = parser:parse()
-	local root = trees[1]:root()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	line = line - 1 -- reindex to 0 to match treesitter
-	local cursor_node = root:descendant_for_range(line, col, line, col)
-	if cursor_node:type() == "comment_content" then return false end
-	return true
-end
-
 MiniDeps.add({
-	source = "saghen/blink.cmp",
+	source = "https://github.com/saghen/blink.cmp",
 	checkout = "v0.11.0",
 })
 
@@ -43,4 +32,4 @@ vim.keymap.set("n", "<leader>cc", function()
 	end
 end, {})
 
-vim.g.autocomplete = false
+vim.g.autocomplete = true
