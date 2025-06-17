5cf9c538db8670e7f06b11cbca7da5891587a263
lukewiebe <mail@lukewiebe.com>
2025-06-17 11:03:14 -0500

    Fix triple backtick markdown code block autoclose
    Instead of trying to use fancy heuristics to detect the differences
    between one and three backticks, simply disable the "escape"
    type-over feature.

--- plugin/autoclose.lua
+++ plugin/autoclose.lua
@@ -4,9 +4,11 @@ require("autoclose").setup({
 	keys = {
 		[">"] = { escape = false, close = false, pair = "><" }, -- autoindent html tags
 		["|"] = { escape = true, close = true, pair = "||", enabled_filetypes = { "zig" } },
+		["`"] = { escape = false, close = true, pair = "``" },
 	},
 
 	options = {
+		disable_when_touch = true,
 		pair_spaces = true,
 	},
 })
---
+++ test/test.md
@@ -0,0 +1,9 @@
+# Hello, World!
+
+- Foo bar baz
+- Hello there
+
+```bash
+set -euo pipefail
+printf "%s %s\n" "hello" "world"
+```
