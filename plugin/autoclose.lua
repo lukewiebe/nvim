a24998464de37f797f9566753e216dd21049c964
lukewiebe <mail@lukewiebe.com>
2025-07-07 11:13:02 -0500

    Add autoclose pairs '$$' for typst

--- plugin/autoclose.lua
+++ plugin/autoclose.lua
@@ -5,6 +5,7 @@ require("autoclose").setup({
 		[">"] = { escape = false, close = false, pair = "><" }, -- autoindent html tags
 		["|"] = { escape = true, close = true, pair = "||", enabled_filetypes = { "zig" } },
 		["`"] = { escape = false, close = true, pair = "``" },
+		["$"] = { escape = true, close = true, pair = "$$", enabled_filetypes = { "typst" } },
 	},
 
 	options = {
