3e3a695af9839951b4d70ed8a7e22e1c7d153fc2
lukewiebe <mail@lukewiebe.com>
2025-07-07 11:14:47 -0500

    One-off script: count java methods in file

---
+++ lua/count_java_methods.lua
@@ -0,0 +1,17 @@
+-- counts the number of methods in a java file. Intended to be called from the command line in headless mode.
+
+local parser = vim.treesitter.get_parser()
+local trees = parser:parse()
+local root = trees[1]:root()
+
+local query = vim.treesitter.query.parse(vim.bo.filetype, [[ (method_declaration) @foo ]])
+
+local count = 0
+for _, _ in query:iter_captures(root, vim.api.nvim_get_current_buf(), 0, -1) do
+	count = count + 1
+end
+
+-- print("number of methods in " .. vim.fn.expand("%:t") .. ": " .. count .. "\n")
+if count > 0 then
+	print(count .. "\n")
+end
