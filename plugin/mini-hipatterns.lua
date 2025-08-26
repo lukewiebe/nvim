943c6db09f003c0373a808c09982e411ca7dc9f6
lukewiebe <mail@lukewiebe.com>
2025-08-26 10:07:05 -0500

    simplify TODO highlight

--- plugin/mini-hipatterns.lua
+++ plugin/mini-hipatterns.lua
@@ -4,10 +4,9 @@ local hipatterns = require("mini.hipatterns")
 
 hipatterns.setup({
   highlighters = {
-    -- Highlight standalone ' TODO:', ' NOTE:'
-    todo = { pattern = "() %f[%w]TODO%f[%W]:()", group = "MiniHipatternsTodo" },
-    -- todo = { pattern = " %f[%w]TODO%f[%W]():()", group = "MiniHipatternsTodoColon" },
-    note = { pattern = "() %f[%w]NOTE%f[%W]:()", group = "MiniHipatternsNote" },
+    -- Highlight standalone 'TODO', 'NOTE'
+    todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+    note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
 
     -- Highlight hex color strings (`#rrggbb`) using that color
     hex_color = hipatterns.gen_highlighter.hex_color(),
