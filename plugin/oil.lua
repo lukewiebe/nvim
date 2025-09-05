714d2df3e8511056cb36173a122bef087151b3aa
lukewiebe <mail@lukewiebe.com>
2025-09-05 12:07:04 -0500

    Hide typst-generated pdfs in Oil

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -1,19 +1,32 @@
 -- https://github.com/stevearc/oil.nvim
 
-require("oil").setup({
+local oil = require("oil")
+
+oil.setup({
   keymaps = {
     ["<C-x>"] = { "actions.select", opts = { horizontal = true } },
     ["y."] = "actions.copy_entry_path",
   },
   view_options = {
     show_hidden = true,
-    is_always_hidden = function(name)
+    is_always_hidden = function(name, bufnr)
       -- if this function returns true for a filename, hide that item
       local ignore = {
         ".DS_Store",
         "..",
       }
-      return vim.tbl_contains(ignore, name)
+      if vim.tbl_contains(ignore, name) then return true end
+
+      -- ignore pdfs if there is a typst file with the same name
+      if name:sub(-4) ~= ".pdf" then return false end
+      local name_no_extension = name:sub(1, -5)
+      local dir = oil.get_current_dir(bufnr)
+
+      for file in vim.fs.dir(dir) do
+        if file:sub(1, -5) == name_no_extension then return true end
+      end
+
+      return false
     end,
   },
 })
