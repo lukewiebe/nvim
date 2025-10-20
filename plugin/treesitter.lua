6299e62b12c454e7c629f54c42d1aa2422626fc7
lukewiebe <mail@lukewiebe.com>
2025-10-20 10:00:33 -0500

    Treesitter: fix error

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -7,31 +7,11 @@ vim.o.foldmethod = "expr" -- overrides 'syntax' in init.lua
 vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { '<filetype>' },
+  pattern = '*',
   callback = function()
+    if not vim.tbl_contains(ts.get_installed(), vim.bo[0].filetype) then return end
     vim.treesitter.start()
     vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
 })
-
--- left out matchup treesitter match words
-
---
---   -- from https://github.com/nvim-treesitter/nvim-treesitter-refactor
---   refactor = {
---     smart_rename = {
---       enable = true,
---       keymaps = {
---         smart_rename = "<leader>s", -- overridden by autocmd on LSP attach
---       },
---     },
---     navigation = {
---       enable = true,
---       keymaps = {
---         goto_definition_lsp_fallback = "gd",
---         list_definitions_toc = "gO",
---       },
---     },
---   },
--- })
