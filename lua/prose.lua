26151edaf267d0a8e27f2ca850464b9982ee451d
Luke Wiebe <mail@lukewiebe.com>
2026-05-19 14:18:40 -0500

    Add gj and gk maps for prose mode

--- lua/prose.lua
+++ lua/prose.lua
@@ -4,5 +4,7 @@
 return function()
   vim.keymap.set({ "n", "v" }, "j", "gj", { buffer = true })
   vim.keymap.set({ "n", "v" }, "k", "gk", { buffer = true })
+  vim.keymap.set({ "n", "v" }, "gj", "j", { buffer = true })
+  vim.keymap.set({ "n", "v" }, "gk", "k", { buffer = true })
   vim.o.linebreak = true
 end
